4f4c066991776fbb25b3f73e2d60af6b3609b080
User-PC\User <hungphat11997@gmail.com>
2019-06-02 23:08:15 +0700

    add

--- db/db.js
+++ db/db.js
@@ -3,18 +3,18 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
 
 
-  // host     : '172.29.64.132',
-  // user     : 'namtv',
-  // password : 'Aa123456',
-  // database : 'cdio_db'
-  // host     : 'localhost',
+  host     : '172.29.64.132',
+  user     : 'namtv',
+  password : 'Aa123456',
+  database : 'cdio_db',
+  host     : 'localhost',
   // user     : 'root',
   // password : 'hoangphupy',
   // database : 'syllabusdb'
-  host     : 'localhost',
-  user     : 'root',
-  password : 'mug3nnsx',
-  database : 'cdio_db'
+  // host     : 'localhost',
+  // user     : 'root',
+  // password : 'mug3nnsx',
+  // database : 'cdio_db'
 
 });
 connection.connect();
