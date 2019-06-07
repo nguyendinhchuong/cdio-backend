3120fe7ebf8b8c61d376df5c6bc40835279881a8
NDChuong <nguyendinhchuong2607@gmail.com>
2019-06-07 16:43:35 +0700

    fix authen role, set username and role in header

--- controllers/User/authController.js
+++ controllers/User/authController.js
@@ -62,23 +62,28 @@ exports.isAuthenticated = (req, res, next) => {
 }
 
 exports.authenRole = (req, res, next) => {
-    let body = JSON.parse(req.body.data);
     let request = {};
-    request.username = body.username;
-    request.role = body.role;
 
-    user.authenRole(request)
-        .then(data => {
-            let response = {};
-            if (data) {
-                next();
-            } else {
-                response.code = -1;
-                response.message = "access denied";
-                res.send(JSON.stringify(response));
-            }
-        })
-        .catch(err => {
-            throw err;
-        })
+    if (req.headers && req.headers.username && req.headers.role) {
+        request.username = req.headers.username;
+        request.role = JSON.parse("[" + req.headers.role + "]");
+        console.log(request);
+        user.authenRole(request)
+            .then(data => {
+                let response = {};
+                if (data) {
+                    next();
+                } else {
+                    response.code = -1;
+                    response.message = "access denied";
+                    res.send(JSON.stringify(response));
+                }
+            })
+            .catch(err => {
+                throw err;
+            })
+    }
+
+
+
 }
