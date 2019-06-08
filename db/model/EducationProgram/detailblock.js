b522900abe42f45e82bd201261d8588716627a6e
NDChuong <nguyendinhchuong2607@gmail.com>
2019-06-08 14:00:10 +0700

    doing export pdf file

--- db/model/EducationProgram/detailblock.js
+++ db/model/EducationProgram/detailblock.js
@@ -19,6 +19,9 @@ module.exports = (sequelize, Sequelize) => {
             allowNull: true
         },
         IdUser:{
+            type: Sequelize.STRING
+        },
+        IDMainTeacher:{
             type: Sequelize.INTEGER
         }
     },{
