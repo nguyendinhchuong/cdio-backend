83c7e3fba37259d1cacddb3da673e874672a4b00
namtv3 <namtv3@vng.com.vn>
2019-06-26 13:44:26 +0700

    Fix name port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const PORT = 30001;
+const PORT = 3001;
 
 var indexRouter = require('./routes/index');
 
